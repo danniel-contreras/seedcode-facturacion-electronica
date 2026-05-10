0f92251d6fb22fd8943e9e1a49126dcbdcf7400d
YeniferCenteno <yenifercentenogr@gmail.com>
2026-05-09 21:47:50 -0600

    remove api

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
-export const MH_DTE_TEST = "https://recepciondte-api.erpseedcodesv.com/api/dtes/recepcion-dte";
-export const MH_DTE = "https://recepciondte-api.erpseedcodesv.com/api/dtes/recepcion-dte";
+export const MH_DTE_TEST = "https://recepciondte-api.erpseedcodesv.com/dtes/recepcion-dte";
+export const MH_DTE = "https://recepciondte-api.erpseedcodesv.com/dtes/recepcion-dte";
 export const CHECK_URL = "https://admin.factura.gob.sv/consultaPublica"
-export const MH_INVALIDATION_TEST = "https://recepciondte-api.erpseedcodesv.com/api/dtes/anular-dte"
-export const MH_INVALIDATION = "https://recepciondte-api.erpseedcodesv.com/api/dtes/anular-dte"
+export const MH_INVALIDATION_TEST = "https://recepciondte-api.erpseedcodesv.com/dtes/anular-dte"
+export const MH_INVALIDATION = "https://recepciondte-api.erpseedcodesv.com/dtes/anular-dte"
